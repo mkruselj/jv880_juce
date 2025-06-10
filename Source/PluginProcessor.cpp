74e897fcbbd6d4ad4faf377a4d30974217eed4e5
Mario Kruselj <mario.kruselj@gmail.com>
2025-06-10 12:04:29 +0200

    Rename official codenames
    "JV-" and "RD-" are removed, and "SR-JV80" is replaced with "Exp".
    
    Also shortened the Keyboards of the 60s and 70s so that it doesn't get too squished on the GUI.
    
    Also removed unused array in PluginProcessor.cpp.

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -9,29 +9,6 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-static const char *expansionFilenames[] = {
-    "rd500_expansion.bin",
-    "SR-JV80-01 Pop - CS 0x3F1CF705.bin",
-    "SR-JV80-02 Orchestral - CS 0x3F0E09E2.BIN",
-    "SR-JV80-03 Piano - CS 0x3F8DB303.bin",
-    "SR-JV80-04 Vintage Synth - CS 0x3E23B90C.BIN",
-    "SR-JV80-05 World - CS 0x3E8E8A0D.bin",
-    "SR-JV80-06 Dance - CS 0x3EC462E0.bin",
-    "SR-JV80-07 Super Sound Set - CS 0x3F1EE208.bin",
-    "SR-JV80-08 Keyboards of the 60s and 70s - CS 0x3F1E3F0A.BIN",
-    "SR-JV80-09 Session - CS 0x3F381791.BIN",
-    "SR-JV80-10 Bass & Drum - CS 0x3D83D02A.BIN",
-    "SR-JV80-11 Techno - CS 0x3F046250.bin",
-    "SR-JV80-12 HipHop - CS 0x3EA08A19.BIN",
-    "SR-JV80-13 Vocal - CS 0x3ECE78AA.bin",
-    "SR-JV80-14 Asia - CS 0x3C8A1582.bin",
-    "SR-JV80-15 Special FX - CS 0x3F591CE4.bin",
-    "SR-JV80-16 Orchestral II - CS 0x3F35B03B.bin",
-    "SR-JV80-17 Country - CS 0x3ED75089.bin",
-    "SR-JV80-18 Latin - CS 0x3EA51033.BIN",
-    "SR-JV80-19 House - CS 0x3E330C41.BIN",
-};
-
 //==============================================================================
 Jv880_juceAudioProcessor::Jv880_juceAudioProcessor()
     : AudioProcessor(
